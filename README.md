d8fdeaf7b6d10c94c2d93c231e02c55a79e0a619
gdjesus <gabriel.jesus@performait.com>
2021-04-24 01:48:39 -0300

    estudo

---
+++ README.md
@@ -0,0 +1,4 @@
+Paradigmas:
+Funcional: 1957
+Procedural: 1968
+OO: 1980/90
---
+++ funcoes/funcao_1.js
@@ -0,0 +1,11 @@
+console.log("teste");
+
+// Function Declaration
+function bomDia() {
+  console.log("Bom dia!");
+}
+
+// Function expression
+const bomDia = function () {
+  console.log("Bom dia!");
+};
